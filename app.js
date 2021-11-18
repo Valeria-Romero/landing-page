ce41d26d48bb05f215bfe1d401727561f519f424
Valeria Romero <valellope@gmail.com>
2021-11-18 12:04:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const server = http.createServer(function (request, response){
         });
     }
     
-    else if(request.url === '/dojos'){
+    else if(request.url === '/dojos/new'){
         fs.readFile('dojos.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
